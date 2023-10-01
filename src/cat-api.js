8b3f8239b00944705cab64727f11d5ff59ca1134
wielembe <e.wielemborek@gmail.com>
2023-10-01 17:56:55 +0200

    init setup

---
+++ src/cat-api.js
@@ -0,0 +1,15 @@
+'use strict';
+import axios from 'axios';
+axios.defaults.headers.common['x-api-key'] =
+  'live_Q12AQTUozJBs0hF5SsrkR30SDG0kwbI5NG2ewXADD49qZgJLqLNVOgpfB8So5eP6';
+
+const errorMessage = document.querySelector('.error');
+
+export const fetchBreeds = () => {
+  return axios
+    .get('https://api.thecatapi.com/v1/breeds')
+    .then(response => {
+      return response.data;
+    })
+    .catch(error => Notify.failure(errorMessage.textContent));
+};
