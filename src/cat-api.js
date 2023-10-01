bfca6c48d5ef184e6016d6a8203ab49346530449
wielembe <e.wielemborek@gmail.com>
2023-10-01 20:49:36 +0200

    first try

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,5 +1,9 @@
 'use strict';
+
+import { Notify } from 'notiflix';
+
 import axios from 'axios';
+
 axios.defaults.headers.common['x-api-key'] =
   'live_Q12AQTUozJBs0hF5SsrkR30SDG0kwbI5NG2ewXADD49qZgJLqLNVOgpfB8So5eP6';
 
@@ -8,6 +12,15 @@ const errorMessage = document.querySelector('.error');
 export const fetchBreeds = () => {
   return axios
     .get('https://api.thecatapi.com/v1/breeds')
+    .then(response => {
+      return response.data;
+    })
+    .catch(error => Notify.failure(errorMessage.textContent));
+};
+
+export const fetchCatByBreed = breedId => {
+  return axios
+    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
     .then(response => {
       return response.data;
     })
